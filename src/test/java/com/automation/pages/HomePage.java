dfe442dacead2ee57de203a1a6e500e43d53deb0
appukuttan619 <shijas002@gmail.com>
2024-08-31 10:22:34 +0530

    Fixed the bug and implement the basic structure

--- src/test/java/com/automation/pages/HomePage.java
+++ src/test/java/com/automation/pages/HomePage.java
@@ -1,2 +1,24 @@
-package com.automation.pages;public class HomePage {
+package com.automation.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+
+    @FindBy(id = "com.etsy.android:id/cancelText")
+    WebElement continueAsGuest;
+
+    @FindBy(id = "com.etsy.android:id/empty_view_tertiary_button")
+    WebElement locationSkip;
+
+    public void openApplication() {
+        if (isDisplayed(continueAsGuest)) {
+            continueAsGuest.click();
+            if (isPresent(locationSkip)) {
+                locationSkip.click();
+
+            }
+        }
+
+    }
 }
--- src/test/java/com/automation/steps/HomeSteps.java
+++ src/test/java/com/automation/steps/HomeSteps.java
@@ -1,11 +1,15 @@
 package com.automation.steps;
 
+import com.automation.pages.HomePage;
 import io.cucumber.java.en.Given;
 
 public class HomeSteps {
 
+    HomePage homePage = new HomePage();
+
     @Given("user opens the application")
     public void user_opens_the_application() {
+        homePage.openApplication();
 
     }
 
