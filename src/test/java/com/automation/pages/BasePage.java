50b3a5c9ee85b326bcb123747b94bb59e569a670
appukuttan619 <shijas002@gmail.com>
2024-09-02 12:04:55 +0530

    implemented scroll and swipe inside basePage

--- src/test/java/com/automation/pages/BasePage.java
+++ src/test/java/com/automation/pages/BasePage.java
@@ -3,9 +3,13 @@ package com.automation.pages;
 import com.automation.utils.DriverManager;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Pause;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.support.PageFactory;
 
 import java.time.Duration;
+import java.util.Collections;
 
 public class BasePage {
     AppiumDriver driver;
@@ -41,6 +45,23 @@ public class BasePage {
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
 
         }
+    }
+
+    public void scrollOrSwipe(int startX , int startY, int endX, int endY){
+
+        PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+
+        Sequence sequence = new Sequence(finger1, 1)
+                .addAction(finger1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY))
+                .addAction(finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(new Pause(finger1, Duration.ofMillis(500)))
+                .addAction(finger1.createPointerMove(Duration.ofMillis(500), PointerInput.Origin.viewport(), endX, endY))
+                .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Collections.singleton(sequence));
+
+        return;
+
     }
 
 }
